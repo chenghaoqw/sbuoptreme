83533e74d346cb677b1aeb0a5e73a17968792554
chenghaoqw <chenghaplki@163.com>
2017-12-15 01:07:33 +0800

    [Add Story]America

--- sim.py
+++ sim.py
@@ -23,13 +23,13 @@ starttime = ""
 baidu = "https://www.baidu.com"
 goodsurl = "http://www.supremenewyork.com/shop/"
 cart = 'http://www.supremenewyork.com/shop/cart'
-checkout = 'https://www.supremenewyork.com/checkout'
+checkout = 'https://www.supremenewyork.com/checkout.json'
 check_post = "utf8=%E2%9C%93&authenticity_token=C5yjONx%2FOI5G%2FTGuLAO%2BEPB14vjOfxe7jOr5B%2FvmK5JGATFTIahelu18RlEMjBRm%2BZQy%2BOE9CGTVunAGNN2N%2FA%3D%3D&credit_card%5Blast_name%5D=hu&credit_card%5Bfirst_name%5D=yang&order%5Bemail%5D=h924429615@gmail.com&order%5Btel%5D=09012592053&order%5Bbilling_state%5D=+%E6%9D%B1%E4%BA%AC%E9%83%BD&order%5Bbilling_city%5D=%E4%B8%9C%E4%BA%AC&order%5Bbilling_address%5D=%E6%9D%BF%E6%A9%8B%E5%8C%BA%E5%A4%A7%E8%B0%B7%E5%8F%A3%E5%8C%97%E7%94%BA18-3&order%5Bbilling_zip%5D=173-0031&same_as_billing_address=1&credit_card%5Btype%5D=cod&credit_card%5Bcnb%5D=4033920041674989&credit_card%5Bmonth%5D=11&credit_card%5Byear%5D=2022&credit_card%5Bvval%5D=909&order%5Bterms%5D=0&order%5Bterms%5D=1&hpcvv=&commit=%E8%B3%BC%E5%85%A5%E3%81%99%E3%82%8B"
 has_cookie = False
 file_name = "cookie"
 stock = "http://www.supremenewyork.com/mobile_stock.json?_="
 property = "http://www.supremenewyork.com/shop/"
-info = "http://1.surpreme.applinzi.com/3"
+info = "http://1.surpreme.applinzi.com/1"
 goods_info = {}
 ids = {}
 isHave = False
@@ -157,30 +157,31 @@ def start_bot():
         cls_shop.checkout(checkout, check_post)
         bot.remove()
         thread.shutdown(wait=False)
-    # else:
-    #     bot.remove()
-    #     thread.shutdown(wait=False)
-
+        # else:
+        #     bot.remove()
+        #     thread.shutdown(wait=False)
 
 
 if __name__ == '__main__':
     cls_shop = shop()
     response = cls_shop.getInfo()
-    print(response)
     j = json.loads(response)
+    print(j)
     if (j['isValid'] != 1):
         sys.exit()
     goods = j['goods']
-    # starttime = j['time']
-    starttime = '2017-12-14 14:50:00'
+    check_post = str(j['commit'])
+    print(check_post)
+    starttime = j['time']
+    # starttime = '2017-12-14 23:46:00'
     for cat in goods:
         goods_info[cat['category']] = cat['name']
 
     thread = BlockingScheduler()
-
-    alive = thread.add_job(alive, 'cron', second='*/5', id='alive',
-                           end_date=datetime.datetime.strptime(starttime, '%Y-%m-%d %H:%M:%S') - datetime.timedelta(
-                               seconds=6))
-    bot = thread.add_job(start_bot, 'cron', id='bot',
-                         start_date=datetime.datetime.strptime(starttime, '%Y-%m-%d %H:%M:%S'))
+    alivetrigger = CronTrigger(second='*/5',
+                               end_date=datetime.datetime.strptime(starttime, '%Y-%m-%d %H:%M:%S') - datetime.timedelta(
+                                   seconds=6))
+    alive = thread.add_job(alive, alivetrigger)
+    bottrigger = CronTrigger(start_date=datetime.datetime.strptime(starttime, '%Y-%m-%d %H:%M:%S'))
+    bot = thread.add_job(start_bot, bottrigger)
     thread.start()
